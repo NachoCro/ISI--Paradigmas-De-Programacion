6e440443577ebdb7c121e463bdb121202ec29ab2
Nacho <Nachossjb@gmail.com>
2026-04-17 19:39:36 -0300

    Hecho en la clase 6

---
+++ NombreYapellido/NombreYapellido/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace NombreYapellido
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Apellido, Nombre, ApellidoyNombre;
+
+            Console.WriteLine("Inserte su apellido: ");
+            Apellido = Console.ReadLine();
+            Console.WriteLine("Inserte su Nombre: ");
+            Nombre = Console.ReadLine();
+
+            if (Juntar(Apellido, Nombre, out ApellidoyNombre))
+            {
+                Console.WriteLine($"Hola {ApellidoyNombre}");
+            }
+            else
+            {
+                Console.WriteLine("No esta completado el nombre o apellido");
+            }
+        }
+
+
+        static bool Juntar(string A, string N, out string AyN)
+        {
+            bool funciona = false;
+            AyN = string.Empty;
+            if (A != string.Empty && N != string.Empty) { 
+                funciona = true;
+                AyN = $"{A}, {N}";
+            }
+            return funciona;
+        }
+    }
+}
---
+++ clase7/clase7/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace clase7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+
+            a = 1;
+            b = 2;
+
+            interCambiar(ref a, ref b);
+
+            Console.WriteLine($"a vale: {a}");
+            Console.WriteLine($"b vale: {b}");
+        }
+
+        static void interCambiar(ref int x, ref int y)
+        {
+            int t;
+            t = x;
+            x = y;
+            y = t;
+
+        }
+
+    }
+}
