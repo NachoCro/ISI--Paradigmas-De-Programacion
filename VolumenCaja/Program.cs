63d19816f015a93aff9849d2f91eb1b401ce4577
arilago <arianalagorio@gmail.com>
2026-04-17 17:39:11 -0300

    Ejercicio 1. hecho

---
+++ VolumenCaja/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace VolumenCaja;
+
+class Program
+{
+
+    static float Insertar(string b)
+    {
+        float a;
+        string cadena;
+        Console.WriteLine($"Inserte {b} de la caja:");
+        cadena = Console.ReadLine();
+        while (float.TryParse(cadena, out a) == false)
+        {
+            Console.WriteLine($"Inserte {b} de la caja:");
+            cadena = Console.ReadLine();
+        }
+        return a;
+    }
+    static void Main(string[] args)
+    {
+
+
+        float Ancho, Profundidad, Alto, Volumen;
+
+        Ancho = Insertar("el ancho");
+        Profundidad =Insertar("la profundidad");
+        Alto = Insertar("el alto");
+
+        Volumen = Ancho*Alto*Profundidad;
+        Console.WriteLine($"El volumen es {Volumen}m³");
+    }
+}
