dcd0ca906ae86453681f98361981c02e5d3cc60e
arilago <arianalagorio@gmail.com>
2026-04-17 17:32:44 -0300

    Ejercicios 2. 3. 4. Hechos

--- TipoTriangulo/Program.cs
+++ TipoTriangulo/Program.cs
@@ -10,27 +10,35 @@
             c = Console.ReadLine();
             while (float.TryParse(c, out a) == false)
             {
-                 Console.WriteLine($"Ingrese el {b} lado de triangulo");
+                Console.WriteLine($"Ingrese el {b} lado de triangulo");
                 c = Console.ReadLine();
             }
 
             return a;
         }
 
-        static bool esTriangulo(float a,float b,float c){
-            if(a <= (b+c) || b <= (a+c) || c <= (b+a)){
+        static bool esTriangulo(float a, float b, float c)
+        {
+            if (a <= (b + c) || b <= (a + c) || c <= (b + a))
+            {
                 return true;
             }
             return false;
         }
 
-        static void TipoTriangulo(float a,float b,float c){
-            if(a == b && a == c){
-                 Console.WriteLine("Es un triangulo equilatero");
-            }else if(a == b || a == c || c == b){
-                 Console.WriteLine("Es un triangulo isoseles");
-            }else{
-                 Console.WriteLine("es un triangulo escaleno");
+        static void TipoTriangulo(float a, float b, float c)
+        {
+            if (a == b && a == c)
+            {
+                Console.WriteLine("Es un triangulo equilatero");
+            }
+            else if (a == b || a == c || c == b)
+            {
+                Console.WriteLine("Es un triangulo isoseles");
+            }
+            else
+            {
+                Console.WriteLine("es un triangulo escaleno");
             }
         }
         static void Main()
@@ -42,12 +50,15 @@
             lado2 = Insertar("segundo");
             lado3 = Insertar("tercer");
 
-            if(esTriangulo(lado1, lado2, lado3)){
+            if (esTriangulo(lado1, lado2, lado3))
+            {
                 TipoTriangulo(lado1, lado2, lado3);
-            }else{
-                 Console.WriteLine("no es un triangulo");
             }
-            
+            else
+            {
+                Console.WriteLine("no es un triangulo");
+            }
+
         }
     }
 }
--- trianguloR/Program.cs
+++ trianguloR/Program.cs
@@ -6,24 +6,31 @@
         {
             float a;
             string c;
-            System.Console.WriteLine($"Ingres el {b} angulo de triangulo");
+            Console.WriteLine($"Ingres el {b} angulo de triangulo");
             c = Console.ReadLine();
             while (float.TryParse(c, out a) == false)
             {
-                System.Console.WriteLine($"Ingrese el {b} angulo de triangulo");
+                Console.WriteLine($"Ingrese el {b} angulo de triangulo");
                 c = Console.ReadLine();
             }
 
             return a;
         }
 
-        static void Validacion(float a,float b,float c){
-            if(a+b+c != 180){
-                System.Console.WriteLine("NO ES UN TRIANGULO");
-            }else if(a == 90 || b == 90 || c == 90){
-                System.Console.WriteLine("Es un triangulo rectangulo");
-            }else{
-                System.Console.WriteLine("No es un triangulo rectangulo");
+        static void Validacion(float a, float b, float c)
+        {
+            if (a + b + c != 180
+            && (a == 180 || c == 180 || b == 180))
+            {
+                Console.WriteLine("NO ES UN TRIANGULO");
+            }
+            else if (a == 90 || b == 90 || c == 90)
+            {
+                Console.WriteLine("Es un triangulo rectangulo");
+            }
+            else
+            {
+                Console.WriteLine("No es un triangulo rectangulo");
             }
         }
 
@@ -36,7 +43,7 @@
             Angulo2 = Insertar("segundo");
             Angulo3 = Insertar("tercer");
 
-            Validacion(Angulo1,Angulo2,Angulo3);
+            Validacion(Angulo1, Angulo2, Angulo3); // C:
         }
     }
 }
