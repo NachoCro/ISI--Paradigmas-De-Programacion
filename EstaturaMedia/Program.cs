84e0c9b4eeea71d7e027f306a7720957263184cf
arilago <arianalagorio@gmail.com>
2026-04-17 17:07:23 -0300

    Ejercicios 1. - 2. - 3.  hechos

--- EstaturaMedia/Program.cs
+++ EstaturaMedia/Program.cs
@@ -1,1 +1,43 @@
-﻿Console.WriteLine("Hello, World!");
+﻿namespace EstaturaMedia;
+
+class Program{
+
+    static float Insertar(string b)
+        {
+            float a;
+            string c;
+            System.Console.WriteLine("{b}");
+            c = Console.ReadLine();
+            while (float.TryParse(c, out a) == false)
+            {
+                System.Console.WriteLine($"{b}");
+                c = Console.ReadLine();
+            }
+
+            return a;
+        }
+
+        static void superaEstatura(string a, float b){
+            if((a.ToLower == "m" || a.ToLower == "masculino") && b > 1.72){
+                System.Console.WriteLine("Sos alto");
+            }if((a.ToLower == "m" || a.ToLower == "masculino") && b < 1.72){
+                System.Console.WriteLine("Sos bajo");
+            }if((a.ToLower == "f" || a.ToLower == "femenino") && b > 1.65){
+                System.Console.WriteLine("Sos alta");
+            }if((a.ToLower == "f" || a.ToLower == "femenino") && b > 1.65){
+                System.Console.WriteLine("Sos baja");
+            }
+        }
+    static void Main(){
+        float Estatura;
+        string genero;
+
+        System.Console.WriteLine("Inserte su sexo: \n > M - masculino \n > F - femenino ");
+        genero = Console.ReadLine();
+
+        Estatura = Insertar("Ingrese su estatura: ");
+
+        superaEstatura(genero, Estatura);
+
+    }
+}
